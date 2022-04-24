5105b8ea19e2f4b8b93364954e1eec009da8b798
Alex Schavareto <schavareto@yahoo.com.br>
2022-04-24 13:47:18 -0300

    Alteração para ler a tabela dados do banco de dados

--- index.php
+++ index.php
@@ -24,111 +24,98 @@ body {background-color: #2F4F4F; color: #FFFFF0}
 	<div class="container">
 
 
-		<?php
+<?php
 		if (isset($_POST['enviar'])){
-
 			if (in_array(NULL, $_POST)) { //troquei os if e else de cada campo do formulário, para a função in_array, que já faz uma mensagem padrão caso algum campo estiver vazio
-		?>
-				<div class="alert alert-primary text-center" role="alert">
-					Algum campo está vazio, volte e preencha novamente.
-				</div>
-				<p>
-					<a href="./">Voltar</a>
-				</p>
+?>
+	<div class="alert alert-primary text-center" role="alert">
+		Algum campo está vazio, volte e preencha novamente.
+	</div>
+	<p>
+		<a href="./">Voltar</a>
+	</p>
 
-		<?php
-			}else{
-		?>
-
-					<?php 
-						require_once("gravar.php"); 
-					?>
-
-				<table class="table table-bordered">
-					<thead>
-						<tr>
-							<th scope="col">ID</th>
-							<th scope="col">Nome</th>
-							<th scope="col">E-mail</th>
-							<th scope="col">Mensagem</th>
-						</tr>
-					</thead>
+<?php
+		}else{				
+?>
 
 <?php 
+			require_once("gravar.php"); 
+?>
 
-    if( file_exists( "dados.txt" ) ) {
-        
-        $fp = fopen( "dados.txt", "r"); // o "r" significa leitura.
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th scope="col">ID</th>
+				<th scope="col">Nome</th>
+				<th scope="col">E-mail</th>
+				<th scope="col">Mensagem</th>
+			</tr>
+		</thead>
 
-        while( !feof( $fp ) ) { //Enquanto não chegar ao final do arquivo declarado na variavel $fp
-            $linha_atual = fgets( $fp );
-            if( !empty( $linha_atual )){ //Esse if serve para não imprimir a linha vazia
-                $ex = explode(";",$linha_atual); //função explode cria um array de cada linha com o separador ";"
-                $i = 1; //criei essa variávei para a coluna ID
+<?php
+
+    $conexao = new mysqli("localhost", "root", "123456", "terminalroot");
+
+    if ( ! $conexao->connect_error ){
+
+        $sql = "SELECT * FROM dados";
+        $resultado = $conexao->query($sql);
+        while ($ex = $resultado->fetch_assoc()){
 
 ?>
 
     <tr>
-		<td><?php echo $i ?> </td>
-        <td><?php echo $ex[0];?></td>
-        <td><?php echo $ex[1];?></td>
-        <td><?php echo $ex[2];?></td>
+		<td><?php echo $ex['id'];?></td>
+        <td><?php echo $ex['nome'];?></td>
+        <td><?php echo $ex['email'];?></td>
+        <td><?php echo $ex['mensagem'];?></td>
     </tr>
 
 <?php
-				$i++;
-            }
-        }
 
-        fclose( $fp );
-
-    }else{
-        echo "Arquivo não existe";
-    }
+			};
+	}else{
+		echo "ERRO! Falha ao conectar";
+	}
 ?>
-					</table>
-					
 
 
+	</table>
+		<p>
+			<a href="./">Voltar</a>
+		</p>
 
-					<p>
-						<a href="./">Voltar</a>
-					</p>
-
-					<?php
-				}
+<?php
+		}
 
 			}else{
-				?>
+?>
 
-				<h1 class="text-center">Formulário de Contato</h1>
+	<h1 class="text-center">Formulário de Contato</h1>
+		<form action="./" method="post">
+			<div class="form-group">
+				<label>Nome</label>
+				<input type="text" name="nome" class="form-control" >
+			</div>
 
-				<form action="./" method="post">
+			<div class="form-group">
+				<label>E-mail</label>
+				<input type="email" name="email" class="form-control" >
+			</div>
 
-					<div class="form-group">
-						<label>Nome</label>
-						<input type="text" name="nome" class="form-control" >
-					</div>
+			<div class="form-group">
+				<label>Mensagem</label>
+				<pre><textarea type="text" name="mensagem" class="form-control"></textarea></pre>
+			</div>
 
-					<div class="form-group">
-						<label>E-mail</label>
-						<input type="email" name="email" class="form-control" >
-					</div>
+			<button type="submit" name="enviar" class="btn btn-primary" value="enviar">Enviar</button>
+		</form>
 
-					<div class="form-group">
-						<label>Mensagem</label>
-						<pre><textarea type="text" name="mensagem" class="form-control"></textarea></pre>
-					</div>
-
-					<button type="submit" name="enviar" class="btn btn-primary" value="enviar">Enviar</button>
-				</form>
-
-				<?php
+<?php
 			}
-			?>
+?>
 
 		</div>
-
-
 	</body>
-	</html>
+</html>
--- teste-mysql.php
+++ teste-mysql.php
@@ -3,15 +3,21 @@
     $conexao = new mysqli("localhost", "root", "123456", "terminalroot");
 
     if ( ! $conexao->connect_error ){
+
         $sql = "SELECT * FROM dados";
-        if( $conexao-> query($sql) === TRUE){
-            echo "Dados inseridos";
-        }else{
-            echo "Falha ao inserir dados";
-        }
+        $resultado = $conexao->query($sql);
+        while ($linha = $resultado->fetch_assoc()){
 
+            echo "ID: " . $linha['id'] . "<br>";
+            echo "Nome: " . $linha['nome'] . "<br>";
+            echo "E-mail: " . $linha['email'] . "<br>";
+            echo "Mensagem: " . $linha['mensagem'] . "<br>";
+            echo "Data/Hora: " . $linha['data'] . "<br>";
 
+            echo "<hr>";
 
+            
+                };
     }else{
         echo "ERRO! Falha ao conectar";
     }
